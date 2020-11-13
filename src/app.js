aa6940d2c8e1885dd870ec7f4d22b4233e5f175d
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2020-11-13 11:50:04 +0100

    deploy to heroku

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ import errorResponse from "./helpers/errorResponse";
 config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(json());
 if (process.env.NODE_ENV === "development") {
